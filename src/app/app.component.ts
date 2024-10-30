d204100d4c60fae79fe9de5edf87ad5d41a06dd7
kareemelsonbaty <71130316+Kareem7777@users.noreply.github.com>
2024-10-31 00:31:07 +0300

    Update app.component.ts
    remove un used imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
-import { ProductCardComponent } from './components/product-card/product-card.component';
-import { ProductFilterComponent } from './components/product-filter/product-filter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, ProductCardComponent, ProductFilterComponent],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
