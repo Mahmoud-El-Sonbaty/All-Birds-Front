b1b6bfcfce8b9438615e7f30e3052994dd8efd04
ahmed mohamed <ahmedalghoul10@gmail.com>
2024-11-01 19:06:10 +0200

    home page And Edits

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  slides2 = [
+    { img: 'images/slide2-image1.jpg',img2: 'images/slide2-image2.jpg', text: "Men's season ready" , text2: "Women's Fall Essentials", active:true },
+    { img: 'images/slide2-image3.jpg',img2: 'images/slide2-image4.jpg', text: "Men's Sporty Styles" , text2: "Women's Athleisure Styles" , active:false },
+    { img: 'images/slide2-image5.jpg',img2: 'images/slide2-image6.jpg', text: "Men's Lightweight Styles" , text2: "Women's Packable Shoes" , active:false },
+  ];
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { ErrorComponent } from './Components/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path:'',component:HomeComponent
+  ,children:[
+
+  // {path:'main',component:MainComponent,title:"main Page"},
+  {path:'home',component:HomeComponent,title:"Home Page"},
+
+
+]},
+
+//wild card route
+{path:'**',component:ErrorComponent,title:"Not found"}
+
+
+];
