53a7198254d28351e8640c8ea736262716ce6d6f
kareemelsonbaty <71130316+Kareem7777@users.noreply.github.com>
2024-11-03 01:07:08 +0200

    moved the checkout component to the app/components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainProductComponent } from './components/main-product/main-product.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 
 export const routes: Routes = [
   { path: 'Collections', component: MainProductComponent, title: 'Collections'},
+  { path: 'Checkout', component: CheckoutComponent, title: 'Collections'},
   // { path :'Product' ,component : MainProductComponent , title : 'Product Page' }
 ];
