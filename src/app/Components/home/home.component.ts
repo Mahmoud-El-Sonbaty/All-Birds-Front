028cd854abeb96e7fa7ad79c8cc6d9d5a5e15f45
ahmed mohamed <ahmedalghoul10@gmail.com>
2024-11-03 12:31:18 +0200

    some style at home page

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -14,4 +14,28 @@ export class HomeComponent {
     { img: 'images/slide2-image3.jpg',img2: 'images/slide2-image4.jpg', text: "Men's Sporty Styles" , text2: "Women's Athleisure Styles" , active:false },
     { img: 'images/slide2-image5.jpg',img2: 'images/slide2-image6.jpg', text: "Men's Lightweight Styles" , text2: "Women's Packable Shoes" , active:false },
   ];
+
+
+  items = [
+    {
+      type: 'image',
+      src: 'images/slide1-image1.jpg',
+      title: 'Wool Runner Go',
+      subtitle: 'Sublimely Soft, Everyday Elevated'
+    },
+    {
+      type: 'video',
+      src: 'images/video.webm',
+      title: 'Wool Runner Mizzle',
+      subtitle: 'Weather-ready, Everyday Sneaker'
+    },
+    {
+      type: 'image',
+      src: 'images/slide1-image2.jpg',
+      title: 'Tree Topper',
+      subtitle: 'Breezy High-Top Sneakers'
+    }
+  ];
+
+
 }
