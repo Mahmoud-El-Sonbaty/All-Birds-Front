cb478f18d253b426ec31705138c54cf1678458a8
ahmed mohamed <ahmedalghoul10@gmail.com>
2024-11-07 04:28:28 +0200

    localization v3

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -6,11 +6,13 @@ import { Subscription } from 'rxjs';
 import { Iproduct } from '../../../Modules/Product';
 import { error } from 'jquery';
 import { environment } from '../../../environments/environment.development';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,SliderCmComponent],
+  imports: [CommonModule,SliderCmComponent,TranslateModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -19,60 +21,101 @@ export class HomeComponent implements OnInit,OnDestroy {
   sub:Subscription[]= []as Subscription[];
   Products:Iproduct[]=[]as Iproduct[];
   imagepath:string=environment.BaseIMageUrl;
-  constructor(private Product:ProductService) {
+  constructor(private Product: ProductService, private translate: TranslateService) {}
 
-  }
-  ngOnDestroy(): void {
-    for (const element of this.sub) {
-      element.unsubscribe();
-  }
-  }
   ngOnInit(): void {
-    var pr = this.Product.GetTop(1,4).subscribe(
-      {
-        next:(res)=>{
-          this.Products=res.data;
-          console.log(this.Products)
+    this.translate.get([
+      'home.3cards.card1.title',
+      'home.3cards.card1.subtitle',
+      'home.3cards.card2.title',
+      'home.3cards.card2.subtitle',
+      'home.3cards.card3.title',
+      'home.3cards.card3.subtitle',
+
+      'home.slider2.slide1.card1',
+      'home.slider2.slide1.card2',
+      'home.slider2.slide2.card1',
+      'home.slider2.slide2.card2',
+      'home.slider2.slide3.card1',
+      'home.slider2.slide3.card2'
+    ]).subscribe(translations => {
+      this.items = [
+        {
+          type: 'image',
+          src: 'Images/slide1-image1.jpg',
+          title: translations['home.3cards.card1.title'],
+          subtitle: translations['home.3cards.card1.subtitle']
         },
-        error:(er)=>{
-
-          console.log(er);
+        {
+          type: 'video',
+          src: 'Images/video.webm',
+          title: translations['home.3cards.card2.title'],
+          subtitle: translations['home.3cards.card2.subtitle']
+        },
+        {
+          type: 'image',
+          src: 'Images/slide1-image2.jpg',
+          title: translations['home.3cards.card2.title'],
+          subtitle: translations['home.3cards.card3.subtitle']
         }
+      ];
+      this.slides2 = [
+        {
+          img: 'Images/slide2-image1.jpg',
+          img2: 'Images/slide2-image2.jpg',
+          text: translations['home.slider2.slide1.card1'],
+          text2: translations['home.slider2.slide1.card2'],
+          active: true
+        },
+        {
+          img: 'Images/slide2-image3.jpg',
+          img2: 'Images/slide2-image4.jpg',
+          text: translations['home.slider2.slide2.card1'],
+          text2: translations['home.slider2.slide2.card2'],
+          active: false
+        },
+        {
+          img: 'Images/slide2-image5.jpg',
+          img2: 'Images/slide2-image6.jpg',
+          text: translations['home.slider2.slide3.card1'],
+          text2: translations['home.slider2.slide3.card2'],
+          active: false
+        }
+      ];
+    });
+
+    // تحميل المنتجات من الخدمة
+    const pr = this.Product.GetTop(1, 4).subscribe({
+      next: (res) => {
+        this.Products = res.data;
+        console.log(this.Products);
+      },
+      error: (er) => {
+        console.log(er);
       }
-    );
+    });
     this.sub.push(pr);
-
-
-
   }
-  slides2 = [
-    { img: 'Images/slide2-image1.jpg',img2: 'Images/slide2-image2.jpg', text: "Men's season ready" , text2: "Women's Fall Essentials", active:true },
-    { img: 'Images/slide2-image3.jpg',img2: 'Images/slide2-image4.jpg', text: "Men's Sporty Styles" , text2: "Women's Athleisure Styles" , active:false },
-    { img: 'Images/slide2-image5.jpg',img2: 'Images/slide2-image6.jpg', text: "Men's Lightweight Styles" , text2: "Women's Packable Shoes" , active:false },
-  ];
 
+      ngOnDestroy(): void {
+        for (const element of this.sub) {
+          element.unsubscribe();
+      }
+  }
+
+
+
+  slides2 = [
+    { img: 'Images/slide2-image1.jpg', img2: 'Images/slide2-image2.jpg', text: "Men's season ready", text2: "Women's Fall Essentials", active: true },
+    { img: 'Images/slide2-image3.jpg', img2: 'Images/slide2-image4.jpg', text: "Men's Sporty Styles", text2: "Women's Athleisure Styles", active: false },
+    { img: 'Images/slide2-image5.jpg', img2: 'Images/slide2-image6.jpg', text: "Men's Lightweight Styles", text2: "Women's Packable Shoes", active: false }
+  ];
 
   items = [
-    {
-      type: 'image',
-      src: 'Images/slide1-image1.jpg',
-      title: 'Wool Runner Go',
-      subtitle: 'Sublimely Soft, Everyday Elevated'
-    },
-    {
-      type: 'video',
-      src: 'Images/video.webm',
-      title: 'Wool Runner Mizzle',
-      subtitle: 'Weather-ready, Everyday Sneaker'
-    },
-    {
-      type: 'image',
-      src: 'Images/slide1-image2.jpg',
-      title: 'Tree Topper',
-      subtitle: 'Breezy High-Top Sneakers'
-    }
+    { type: 'image', src: 'Images/slide1-image1.jpg', title: '', subtitle: '' },
+    { type: 'video', src: 'Images/video.webm', title: '', subtitle: '' },
+    { type: 'image', src: 'Images/slide1-image2.jpg', title: '', subtitle: '' }
   ];
-
 
 
 
