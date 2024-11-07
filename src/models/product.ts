c6d71604372e4ef27c2ced08654d1ca46486f7eb
kareemelsonbaty <71130316+Kareem7777@users.noreply.github.com>
2024-11-07 12:28:39 +0200

    fix merge conflicts

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,29 +1,47 @@
 export interface IProduct {
-  id: number;
-  nameEn: string;
-  nameAr: string;
-  price: number;
-  productColors: IproductColor[];
-}
-
-export interface ApiresponsePrd {
-  msg: string;
-  isSuccess: boolean;
-  data: IProduct[];
-}
-
-export interface IproductColor
-{
-  productColorId : number;
-  imagePath :string;
-  nameEn : string
-  nameAr : string
-  code: string
-  productSizes :IproductSize[];
-}
-
-export interface IproductSize {
-  productColorSizeId: number;
-  sizeNumber: string;
-  unitsInStock : number;
-}
+    id: number;
+    nameEn: string;
+    nameAr: string;
+    price: number;
+    productColors: IproductColor[];
+  }
+  
+  export interface Iproduct {
+    id:number,
+    mainImagePath :string
+    price :number
+    colorNameEn :string
+    colorNameAr :string
+    nameAr :string
+    nameEn :string
+  
+  }
+  
+  export interface ApiresponseProduct{
+    data:Iproduct[];
+    msg:string;
+    isSuccess:boolean;
+  }
+  
+  export interface ApiresponsePrd {
+    msg: string;
+    isSuccess: boolean;
+    data: IProduct[];
+  }
+  
+  export interface IproductColor
+  {
+    productColorId : number;
+    imagePath :string;
+    nameEn : string
+    nameAr : string
+    code: string
+    productSizes :IproductSize[];
+  }
+  
+  export interface IproductSize {
+    productColorSizeId: number;
+    sizeNumber: string;
+    unitsInStock : number;
+  }
+  
