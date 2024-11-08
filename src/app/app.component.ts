8ccdefff9279f82ef02f272f2d8a7df147e4c9f0
Mostafa Mahmoud <mostafamahmoud4812a@gmail.com>
2024-11-08 19:26:38 +0200

    Add RTL direction and services for localization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 import { ProductDetailsComponent } from "./Components/product-details/product-details.component";
+import { TranslationService } from '../services/translation.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavBarComponent, ProductDetailsComponent],
   templateUrl: './app.component.html',
+
   styleUrl: './app.component.css',
 })
 
 export class AppComponent {
   title = 'FrontAllbirds';
+  constructor(private translationService: TranslationService) {}
+
+  toggleDirection(): void {
+    this.translationService.toggleDirection();
+  }
+
+  getDirection(): string {
+    return this.translationService.getCurrentDirection();
+  }
 }
