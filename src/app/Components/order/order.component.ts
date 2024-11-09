e19bca088e719da26135f848d5b2b6591164e29f
ahmed mohamed <ahmedalghoul10@gmail.com>
2024-11-09 18:45:51 +0200

    order and orders and stepper and loader

---
+++ src/app/Components/order/order.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Order } from '../../../Modules/orders';
+import { environment } from '../../../environments/environment.development';
+import { Route, Router } from '@angular/router';
+import { StepperComponent } from '../stepper/stepper.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-order',
+  standalone: true,
+  imports: [StepperComponent,CommonModule],
+  templateUrl: './order.component.html',
+  styleUrl: './order.component.css'
+})
+export class OrderComponent {
+  @Input() order: Order = {} as Order;
+  baseImagePath: string = environment.BaseIMageUrl;
+  constructor(private router: Router) {}
+  navigateTo(prdId: number) {
+    this.router.navigateByUrl(`single-product`);
+  }
+}
