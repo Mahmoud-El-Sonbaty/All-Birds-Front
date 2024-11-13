a9432bf74e42c7b0de56360945594e40d326875a
Kareem <mahmoudelsonbaty777@gmail.com>
2024-11-14 01:33:40 +0200

    order service and ts fixes
    get orders of user correct the endpoint
    check if the the date not sent with the orders from the api

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -3,16 +3,17 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment.development';
 import { OrdersData } from '../models/orders';
+import { LanguageService } from './language.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService  {
 
-  constructor(private httpclient: HttpClient) { }
+  constructor(private httpclient: HttpClient, private lang: LanguageService) { }
 
   getUserOrders(userToken: string):Observable<OrdersData>{
-    return this.httpclient.get<OrdersData>(`${environment.BaseUrl}/Order/getallclientorders`,{
+    return this.httpclient.get<OrdersData>(`${environment.BaseUrl}/Order/getallclientorders/${this.lang.getLanguage()}`,{
       headers:{
         'Authorization': "Bearer " + userToken
       }
