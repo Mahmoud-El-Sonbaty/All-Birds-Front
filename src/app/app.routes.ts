c011ff2bc8dc3fc159dc2ca54acd790df2de8f9b
Ahmed Maklad <ahmed.mostafa.makled@gmail.com>
2024-11-13 19:29:26 +0200

    Deleted Components and Service and Image And Models

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { MainProductComponent } from './components/main-product/main-product.component';
 import { SearchComponent } from './components/Search/search/search.component';
 
-
 export const routes: Routes = [
   { path: 'ProductCategory/:id', component: MainProductComponent, title: 'Product Page' },
   { path: 'search', component: SearchComponent, title: 'Search' }
