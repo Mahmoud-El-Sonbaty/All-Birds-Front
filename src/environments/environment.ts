570aa75fff1763d89e32969bc4f9454628080a03
ahmed mohamed <ahmedalghoul10@gmail.com>
2024-11-19 16:34:18 +0200

    ee

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  BaseUrl:'https://allbirdsapi.tryasp.net/api',
-  baseImageUrl: 'https://allbirdsdashboard.tryasp.net/',
+  BaseUrl:'http://allbirdsapi.tryasp.net/api',
+  baseImageUrl: 'http://allbirdsdashboard.tryasp.net/',
 };
